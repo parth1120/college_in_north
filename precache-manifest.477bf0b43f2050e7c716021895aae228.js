3990153da5ae9611ba4fe7eb3ab1978297d3ebac
Parth Shrivastava <parth.shrivastava11@gmail.com>
2020-08-10 23:51:45 +0530

    Updates

---
+++ precache-manifest.477bf0b43f2050e7c716021895aae228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bb0762f9286e6c53234446cb8496a9",
+    "url": "/college_in_north/index.html"
+  },
+  {
+    "revision": "dffecebb49f6f8809798",
+    "url": "/college_in_north/static/css/main.cff49af4.chunk.css"
+  },
+  {
+    "revision": "ebd16bd3d26dda388366",
+    "url": "/college_in_north/static/js/2.b4afdbc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/college_in_north/static/js/2.b4afdbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffecebb49f6f8809798",
+    "url": "/college_in_north/static/js/main.e8d604fd.chunk.js"
+  },
+  {
+    "revision": "a3bd5ea90fc019f4b376",
+    "url": "/college_in_north/static/js/runtime-main.e49939c1.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/college_in_north/static/media/college_02.619ee97e.jpg"
+  }
+]);
